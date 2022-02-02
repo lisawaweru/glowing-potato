54de962efcb3c535d8f3d53fdef33aec4619eaa3
llisawaweru <lisaannewaweru@gmail.com>
2022-02-02 10:56:33 -0600

    first version

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ import input from 'input';
 async function main() {
     const myAge = 19
     const age = await input.text(' What is your age?');
+    const hasLicense = await input.confirm('Do you have your License?');
+    if (myAge >= 19) {
+        console.log('You can drive!');
+    } else {
+        console.log ('You cannnot drive!'); 
+        if (!hasLicense) {
+            console.log("Go get your license!")
+        } else {
+            console.log('You are not old enough!');
+        }
+    }
+}
 
-if (myAge >= 19) {
-console.log('You can drive!');
-} else {
-console.log ('You cannnot drive!'); 
-}
-}
 main();
